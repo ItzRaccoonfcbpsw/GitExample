b5ce472f239712b44fda4319277f7300dab8ad99
Isaias De Jesus Lalinde Angarita <ilalinde@uninorte.edu.co>
2025-03-17 08:51:14 -0500

    Feat: Added Person class

---
+++ src/core/Person.java
@@ -0,0 +1,12 @@
+package core;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+
+    }
+}
--- src/main.java
+++ src/main.java
@@ -2,5 +2,8 @@
 public class main {
         public static void main(String[] args) {
         System.out.println("Hola mundo");
+        
+        Person person = new Person(54646546, "Isaias", 18);
+            System.out.println(person);
     }
 }
